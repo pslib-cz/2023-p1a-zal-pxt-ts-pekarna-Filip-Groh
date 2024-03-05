f8f7c864c49db87c93522d069368c03dbc8ce992
Filip Groh <146720108+Filip-Groh@users.noreply.github.com>
2024-03-05 10:00:46 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -13,17 +13,28 @@ type Odber = {
     cas: number, //čas je v sekundách
     spotreba: number //spotřeba ve watech za hodinu
 }
-const cena: number = 6.70; // Kč / kWh
+const cena: number = 6.70 // Kč / kWh
 let data: Array<Odber> = [
     { cas: 600, spotreba: 3500 },
     { cas: 120, spotreba: 0 },
     { cas: 300, spotreba: 1700 }, // data[2].cas
     { cas: 60, spotreba: 0 },
-    { cas: 800, spotreba: 1500 },
+    { cas: 800, spotreba: 1500 }
 ]
 
-for (const peceme of data){
-    console.logValue("čas: ", peceme.cas);
-    console.logValue("spotřeba: ", peceme.spotreba);
-    console.logValue("spotřebováno: ", peceme.spotreba / 3600 * peceme.cas);
+let celkovaEnergie: number = 0
+let celkovyCas: number = 0
+
+for (const peceme of data) {
+    console.logValue("čas: ", peceme.cas)
+    console.logValue("spotřeba: ", peceme.spotreba)
+    console.logValue("energie: ", peceme.spotreba * peceme.cas / 3600)
+    console.logValue("cena: ", peceme.spotreba / 1000 * peceme.cas / 3600 * cena)
+
+    celkovaEnergie += peceme.spotreba / 1000 * peceme.cas / 3600
+    celkovyCas += peceme.cas / 3600
 }
+
+console.logValue("celková energie: ", celkovaEnergie)
+console.logValue("průměrná energie: ", celkovaEnergie / celkovyCas)
+console.logValue("celková cena: ", celkovaEnergie * cena)
